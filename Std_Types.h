47ccd982192705a8ca9b561f2777629af5fbcb48
Mohamed-AbdElfatah19 <62775404+Mohamed-AbdElfatah19@users.noreply.github.com>
2021-03-11 14:14:02 +0200

    Update Std_Types.h

--- Std_Types.h
+++ Std_Types.h
@@ -1,1 +1,16 @@
+/**
+ * @file Std_Types.h
+ * @author Mohamed Abd Elfatah
+ * @brief File containing on defined types
+ *
+ */
+
+#ifndef STD_TYPES_H_INCLUDED
+#define STD_TYPES_H_INCLUDED
+
+typedef unsigned char tByte ;
+typedef unsigned int  tWord ;
+
+
+#endif // STD_TYPES_H_INCLUDED
 
